8bb04302be40bff8ef94fa916dbf6766b04e55a9
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-17 17:03:38 -0400

    added name to basics and updated html

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let nameHolder = document.getElementById("title-name");
+let basicsName = document.getElementById("full-name");
 function reqListener() {
   let data = JSON.parse(this.responseText);
   let name = document.createTextNode(data.name);
   let h1Name = document.createElement("h1");
   h1Name.appendChild(name);
   nameHolder.appendChild(h1Name);
+  basicsName.appendChild(name);
+
+  
+
+  console.log(data.html_url);
+
 }
 
 let req = new XMLHttpRequest();
