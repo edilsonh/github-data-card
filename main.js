0d05f552f938238a7388387f193c92c65cb3b761
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-17 17:33:18 -0400

    added name to title and basics

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-let nameHolder = document.getElementById("title-name");
+let nameTitle = document.getElementById("title-name");
 let basicsName = document.getElementById("full-name");
+
+
 function reqListener() {
   let data = JSON.parse(this.responseText);
   let name = document.createTextNode(data.name);
-  let h1Name = document.createElement("h1");
-  h1Name.appendChild(name);
-  nameHolder.appendChild(h1Name);
-  basicsName.appendChild(name);
+  let name2 = document.createTextNode(data.name);
+  nameTitle.appendChild(name);
+  basicsName.appendChild(name2);
 
-  
+
 
   console.log(data.html_url);
 
