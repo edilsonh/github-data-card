e5b5b8782cb5907a8a793f2ca930626838304b86
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-18 09:14:56 -0400

    updated js data

--- main.js
+++ main.js
@@ -7,25 +7,12 @@ let pic = document.getElementById("avatar")
 
 function reqListener() {
   let data = JSON.parse(this.responseText);
-  let name = document.createTextNode(data.name);
-  let name2 = document.createTextNode(data.name);
-  nameTitle.appendChild(name);
-  basicsName.appendChild(name2);
-
-  let githubName = document.createTextNode(data.login);
-  ghName.appendChild(githubName);
-
-  let company = document.createTextNode(data.company);
-  comp.appendChild(company);
-
-  let website = document.createTextNode(data.blog);
-  webs.appendChild(website);
-
-  let image = document.createElement("img");
-  let avatar = document.createTextNode(data.avatar_url);
-  image.setAttribute("src", data.avatar_url);
-  pic.appendChild(image);
-  console.log(data.avatar_url);
+  nameTitle.innerHTML = `<h1>${data.name}</h1>`;
+  basicsName.innerHTML = `${data.name}`;
+  ghName.innerHTML = `${data.login}`;
+  comp.innerHTML = `${data.company}`;
+  webs.innerHTML = `${data.blog}`;
+  pic.innerHTML = `<img src=${data.avatar_url}>`
 }
 
 let req = new XMLHttpRequest();
