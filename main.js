686dcdf89f08c49fcdb9ea86c7afe48d02d1dccb
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-18 13:37:18 -0400

    adding styling to page

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-let nameTitle = document.getElementById("title-name");
-let basicsName = document.getElementById("full-name");
-let ghName = document.getElementById("gh-name");
-let comp = document.getElementById("company");
-let webs = document.getElementById("website");
-let pic = document.getElementById("avatar")
+let container = document.querySelector("body");
 
 function reqListener() {
   let data = JSON.parse(this.responseText);
-  nameTitle.innerHTML = `<h1>${data.name}</h1>`;
-  basicsName.innerHTML = `${data.name}`;
-  ghName.innerHTML = `${data.login}`;
-  comp.innerHTML = `${data.company}`;
-  webs.innerHTML = `${data.blog}`;
-  pic.innerHTML = `<img src=${data.avatar_url}>`
+
+  let template =
+  `
+  <section class="header" id="title-name">
+    <h1>${data.name}</h1>
+  </section>
+  <section id="content-container" class="body">
+    <section id="basics">
+      <h2>The Basics</h2>
+      <p>Name: <span id="full-name">${data.name}</span></p>
+      <p>Github URL <span id="gh-name">${data.login}</span></p>
+      <p>Company: <span id="company">${data.company}</span></p>
+      <p>Website: <span id="website">${data.blog}</span></p>
+    </section>
+    <section id="story">
+      <h2>The Story</h2>
+      <p id="story-body">qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty</p>
+    </section>
+    <section id="avatar">
+      <img src=${data.avatar_url}>
+
+    </section>
+  </section>
+  `;
+  container.innerHTML = template;
 }
 
 let req = new XMLHttpRequest();
