48d0b7794442a11dcda38549b41b7562a06b696b
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-17 22:19:32 -0400

    added all content on html

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let nameTitle = document.getElementById("title-name");
 let basicsName = document.getElementById("full-name");
-
+let ghName = document.getElementById("gh-name");
+let comp = document.getElementById("company");
+let webs = document.getElementById("website");
+let pic = document.getElementById("avatar")
 
 function reqListener() {
   let data = JSON.parse(this.responseText);
@@ -9,10 +12,20 @@ function reqListener() {
   nameTitle.appendChild(name);
   basicsName.appendChild(name2);
 
+  let githubName = document.createTextNode(data.login);
+  ghName.appendChild(githubName);
 
+  let company = document.createTextNode(data.company);
+  comp.appendChild(company);
 
-  console.log(data.html_url);
+  let website = document.createTextNode(data.blog);
+  webs.appendChild(website);
 
+  let image = document.createElement("img");
+  let avatar = document.createTextNode(data.avatar_url);
+  image.setAttribute("src", data.avatar_url);
+  pic.appendChild(image);
+  console.log(data.avatar_url);
 }
 
 let req = new XMLHttpRequest();
